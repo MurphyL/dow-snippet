6c544e189761eb25d9351ce98f3b4dc51aee685c
hao.luo <1qaz!QAZ1qaz>
2020-11-19 21:30:04 +0800

    update

---
+++ src/core/aside/aside.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+import './aside.css';
+
+
+const ASide = ({ navi = {}, cate }) => {
+    return (
+        <aside>
+            <ul label="LABELS">
+                { (Object.keys(navi) || []).map((key, i) => {
+                    return (
+                        <li key={ i } className={ cate === key ? 'selected' : 'x' }>
+                            <Link to={ `/cat/${key}` }>{ navi[key] }</Link>
+                        </li>
+                    );
+                }) }
+            </ul>
+            <ul label="LINKS">
+                <li>
+                    <a href="/" target="_blank">返回主页</a>
+                </li>
+            </ul>
+        </aside>
+    );
+};
+
+export default ASide;
---
+++ src/core/doc/document.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Board = () => {
+    return (
+        <div>x</div>
+    );
+}
+
+export default Board;
