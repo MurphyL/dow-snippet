6ecdc25bd92ec65050a93c82d76ced1c98e15882
MurphyL <murphyl@outlook.com>
2021-01-04 10:51:13 +0800

    文档模块 - POST模块化替代

--- src/plug/post/post.jsx
+++ src/plug/post/post.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 
-import axios from 'axios';
 
 import Markdown from 'markdown-to-jsx';
 
@@ -8,44 +7,37 @@ import SimpleIcons from 'utils/icons.jsx';
 
 import { markdownOptions } from 'plug/mark/mark.jsx';
 
-import { Loadable } from 'plug/loading/loading.jsx';
+import { AjaxLoadable } from 'plug/loading/loading.jsx';
 
 import './post.css';
 
 const Post = ({ details = {}, toggleNavi }) => {
-    const [ post, setPostInfo ] = useState({ status: 1 });
-    useEffect(() => {
-        axios.get(`${details.u}.md`).then(({ status, data }) => {
-            setPostInfo({
-                 ...(status === 200 ? { status: 0 } : { status: 2, message: '请求数据失败' }), 
-                 content: data 
-            });
-        }).catch(() => {
-            setPostInfo({ status: 2, message: '调用接口失败' });
-        });
-    }, [ details.u ]);
     return (
         <article>
-            <Loadable status={ post.status } message="数据加载中……">
-                <header className="header">
-                    <div className="label">
-                        <span className="icon">
-                            <SimpleIcons icon={ details.i || 'dunked' } />
-                        </span>
-                        <span className="text">{ details.t }</span>
-                    </div>
-                    <div className="operations">
-                        <span onClick={ toggleNavi }>
-                            <SimpleIcons icon="menu" />
-                        </span>
-                    </div>
-                </header>
-                <section className="board">
-                    <div className="markdown-to-jsx">
-                        <Markdown children={ post.content || '' } options={ markdownOptions } />
-                    </div>
-                </section>
-            </Loadable>
+            <AjaxLoadable url={ `${details.u}.md` } render={ (postContent) => {
+                return (
+                    <Fragment>
+                        <header className="header">
+                            <div className="label">
+                                <span className="icon">
+                                    <SimpleIcons icon={ details.i || 'dunked' } />
+                                </span>
+                                <span className="text">{ details.t }</span>
+                            </div>
+                            <div className="operations">
+                                <span onClick={ toggleNavi }>
+                                    <SimpleIcons icon="menu" />
+                                </span>
+                            </div>
+                        </header>
+                        <section className="board">
+                            <div className="markdown-to-jsx">
+                                <Markdown children={ postContent || '' } options={ markdownOptions } />
+                            </div>
+                        </section>
+                    </Fragment>
+                )
+            } }/>
         </article>
     );
 }
